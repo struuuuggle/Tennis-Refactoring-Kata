bb317b67518f4c9fe58a64c7c4162ef7cb4bcb5b
Mikiya Abe <struuuuggle@gmail.com>
2020-02-02 19:15:56 +0900

    Game 1 is over

--- swift/Tennis/TennisGame1.swift
+++ swift/Tennis/TennisGame1.swift
@@ -1,83 +1,104 @@
 import Foundation
 
 class TennisGame1: TennisGame {
-    private let player1: String
-    private let player2: String
-    private var score1: Int
-    private var score2: Int
+    private var player1: Player
+    private var player2: Player
     
     required init(player1: String, player2: String) {
-        self.player1 = player1
-        self.player2 = player2
-        self.score1 = 0
-        self.score2 = 0
+        self.player1 = Player(name: player1)
+        self.player2 = Player(name: player2)
     }
-
+    
     func wonPoint(_ playerName: String) {
-        if playerName == "player1" {
-            score1 += 1
+        if player1.name == playerName {
+            player1.score += 1
         } else {
-            score2 += 1
+            player2.score += 1
         }
     }
     
     var score: String? {
-        var score = ""
-        var tempScore = 0
-        if score1 == score2
-        {
-            switch score1
-            {
-            case 0:
-                score = "Love-All"
-
-            case 1:
-                score = "Fifteen-All"
-
-            case 2:
-                score = "Thirty-All"
-
-            default:
-                score = "Deuce"
-                
-            }
+        
+        guard !GameState.isRunning(player1, player2) else {
+            return GameState.running(player1, player2).score
         }
-        else if score1>=4 || score2>=4
-        {
-            let minusResult = score1-score2
-            if minusResult==1 { score = "Advantage player1" }
-            else if minusResult  == -1 { score = "Advantage player2" }
-            else if minusResult>=2 { score = "Win for player1" }
-            else { score = "Win for player2" }
+
+        guard !GameState.isInDeuce(player1, player2) else {
+            return GameState.inDeuce(player1, player2).score
         }
-        else
-        {
-            for i in 1..<3
-            {
-                if i==1 { tempScore = score1 }
-                else { score = "\(score)-"; tempScore = score2 }
-                switch tempScore
-                {
-                case 0:
-                    score = "\(score)Love"
+        
+        return GameState.end(player1, player2).score
+    }
+}
 
-                case 1:
-                    score = "\(score)Fifteen"
-
-                case 2:
-                    score = "\(score)Thirty"
-
-                case 3:
-                    score = "\(score)Forty"
-
-                default:
-                    break
-
-                }
-            }
+extension TennisGame1 {
+    struct Player {
+        let name: String
+        var score: Int
+        
+        init(name: String) {
+            self.name = name
+            self.score = 0
         }
-        return score
     }
     
+    enum GameState {
+        case running(_ player1: Player, _ player2: Player)
+        case inDeuce(_ player1: Player, _ player2: Player)
+        case end(_ player1: Player, _ player2: Player)
+        
+        var score: String {
+            switch self {
+                
+            case .running(let player1, let player2) where GameState.isAll(player1, player2) && 0...2 ~= player1.score:
+                return "\(TennisScore(player1.score).rawValue)-All"
+
+            case .running(let player1, let player2) where GameState.isAll(player1, player2):
+                return "Deuce"
+                
+            case .running(let player1, let player2):
+                return TennisScore(player1.score).rawValue + "-" + TennisScore(player2.score).rawValue
+                
+            case .inDeuce(let player1, let player2) where GameState.isAll(player1, player2):
+                return "Deuce"
+                
+            case .inDeuce(let player1, let player2):
+                let advantagePlayer = player1.score > player2.score ? player1 : player2
+                return "Advantage \(advantagePlayer.name)"
+                
+            case .end(let player1, let player2):
+                let winner = player1.score > player2.score ? player1 : player2
+                return "Win for \(winner.name)"
+            }
+        }
+                        
+        static func isAll(_ player1: Player, _ player2: Player) -> Bool {
+            player1.score == player2.score
+        }
+        
+        static func isRunning(_ player1: Player, _ player2: Player) -> Bool {
+            !(player1.score >= 4 || player2.score >= 4)
+        }
+        
+        static func isInDeuce(_ player1: Player, _ player2: Player) -> Bool {
+            return abs(player1.score - player2.score) < 2
+        }
+    }
     
+    enum TennisScore: String {
+        case Love
+        case Fifteen
+        case Thirty
+        case Forty
+        
+        init(_ score: Int) {
+            switch score {
+            case 0: self = .Love
+            case 1: self = .Fifteen
+            case 2: self = .Thirty
+            case 3: self = .Forty
+            default: fatalError()
+            }
+        }
+    }
 }
